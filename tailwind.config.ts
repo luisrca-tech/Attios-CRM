e4f7f99e671e0c12338e3443fdb1daf0fc8feaf7
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-15 07:29:11 -0300

    🚨 Fix: fix warning from tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,13 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.css",
+  ],
   theme: {
     extend: {
       fontFamily: {
