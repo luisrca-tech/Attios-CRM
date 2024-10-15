33fe8c3621eaea60a4be5b5d52ef807b3a769b24
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-15 16:46:06 -0300

    💄 Style: configure all colors file using alpha values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
@@ -7,6 +7,28 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
+    colors: {
+      black: "rgb(var(--black) / <alpha-value>)",
+      white: {
+        100: "rgb(var(--white-100) / <alpha-value>)",
+        200: "rgb(var(--white-200) / <alpha-value>)",
+        300: "rgb(var(--white-300) / <alpha-value>)",
+      },
+      primary: {
+        100: "rgb(var(--primary-100) / <alpha-value>)",
+        200: "rgb(var(--primary-200) / <alpha-value>)",
+      },
+      secondary: {
+        100: "rgb(var(--secondary-100) / <alpha-value>)",
+        200: "rgb(var(--secondary-200) / <alpha-value>)",
+        300: "rgb(var(--secondary-300) / <alpha-value>)",
+        400: "rgb(var(--secondary-400) / <alpha-value>)",
+        500: "rgb(var(--secondary-500) / <alpha-value>)",
+      },
+      outline: {
+        100: "rgb(var(--outline-100) / <alpha-value>)",
       },
     },
   },
