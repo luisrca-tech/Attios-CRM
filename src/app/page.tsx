2d11c27a9c1e3d3438247658995f64b261fbfc5f
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-15 18:40:18 -0300

    💄 Style: configure default sizes follow design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main>
-        <h1 className="font-extrabold">Attios CRM</h1>
-        <h2 className="font-normal">Attios CRM</h2>
-        <h3 className="font-light">Attios CRM</h3>
+        <h1 className="text-3xl font-extrabold">Attios CRM</h1>
+        <h2 className="text-xs font-normal">Attios CRM</h2>
+        <h3 className="text-base font-light">Attios CRM</h3>
       </main>
     </HydrateClient>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.625rem",
+      "3xl": "2rem",
+    }
   },
   plugins: [],
 } satisfies Config;
