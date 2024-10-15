3cda464425e37bca6ab383dd5819f36d14816e92
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-15 16:46:45 -0300

    🧪 Feat: test colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main>
-        <h1>Attios CRM</h1>
+        <h1 className="text-primary-100">Attios CRM</h1>
+        <h1 className="text-black">Attios CRM</h1>
+        <h1 className="text-primary-200">Attios CRM</h1>
+        <h1 className="text-secondary-400">Attios CRM</h1>
       </main>
     </HydrateClient>
   );
