4ca8e0f467d63bd0d1549a41e78e9bf06b51e3b8
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-18 16:31:47 -0300

    🚧 Test inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,34 @@
-import { Button } from "~/components/ui/Button";
 import { HydrateClient } from "~/trpc/server";
 
-import Image from "next/image";
-import TestIcon from "/public/icons/ico.png";
+import { Input } from "~/components/ui/Input";
+import InputError from "/public/icons/inputError.png";
+import inputIcon from "/public/icons/inputIcon.png";
+
+const options = ["Option 1", "Option 2", "Option 3"];
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex max-w-48 flex-col gap-1">
-        <h1 className="font-extrabold text-3xl text-primary-100">Attios CRM</h1>
-        <Button disabled>
-          Disabled
-          <Image src={TestIcon} alt="Test Icon" />
-        </Button>
-        <Button>
-          <Image src={TestIcon} alt="Test Icon" />
-        </Button>
-        <Button color="secondary">Click me</Button>
-        <Button color="tertiary">Click me</Button>
-        <Button color="quaternary">
-          <Image src={TestIcon} alt="Test Icon" />
-          Click me
-        </Button>
-        <Button color="quinternary">Click me</Button>
-        <Button color="senary">
-          <Image src={TestIcon} alt="Test Icon" />
-          Click me
-          <Image src={TestIcon} alt="Test Icon" />
-        </Button>
-        <Button disabled variant="outlined" color="primary">
-          disabled
-        </Button>
-        <Button variant="outlined" color="primary">
-          Click me
-        </Button>
-        <Button variant="outlined" color="secondary">
-          Click me
-        </Button>
-        <Button variant="outlined" color="tertiary">
-          Click me
-        </Button>
-        <Button variant="outlined" color="quaternary">
-          Click me
-        </Button>
-        <Button variant="outlined" color="quinternary">
-          Click me
-        </Button>
-        <Button variant="outlined" color="senary">
-          Click me
-        </Button>
+      <main className="flex flex-col gap-1">
+        <Input.Root className="relative max-w-[23.5rem]" fieldText="Name">
+          <Input.Input
+            iconLeft={inputIcon}
+            className="pl-4"
+            placeholder="Type..."
+          />
+        </Input.Root>
+        <Input.Root error className="relative max-w-[23.5rem]" fieldText="Name">
+          <Input.Input iconRight={InputError} placeholder="Type..." />
+        </Input.Root>
+        <Input.Root fieldText="Choose an option" className="max-w-[23.5rem]">
+          <Input.SelectInput>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </Input.SelectInput>
+        </Input.Root>
       </main>
     </HydrateClient>
   );
