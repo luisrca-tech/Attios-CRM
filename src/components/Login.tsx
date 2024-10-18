5b928a3cc9934429a42e064ed5290b5d873f52e0
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-18 13:26:04 -0300

    🚧 Work in progress.

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+export function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-3">
+        <h1 className='max-w-[25.2rem] font-bold text-3xl text-black leading-[2.625rem]'>
+          Welcome to our CRM. Sign Up to getting started.
+        </h1>
+        <span className='font-normal text-sm leading-5'>Enter your details to proceed further</span>
+      </div>
+    </div>
+  );
+}
