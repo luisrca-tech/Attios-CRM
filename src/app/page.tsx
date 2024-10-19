07b4318511d59e79d9f586857db7556ea16cfae9
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-19 11:04:45 -0300

    🚧 Test buttons and inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,45 @@
 import { HydrateClient } from "~/trpc/server";
 
+import { InputIcon } from "~/components/Icons/Input";
+import { Icon } from "~/components/Icons/User";
 import { Input } from "~/components/ui/Input";
-import InputError from "/public/icons/inputError.png";
-import inputIcon from "/public/icons/inputIcon.png";
-
-const options = ["Option 1", "Option 2", "Option 3"];
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex flex-col gap-1">
         <Input.Root className="relative max-w-[23.5rem]" fieldText="Name">
-          <Input.Input
-            iconLeft={inputIcon}
+          <Input.Text
+            id="name"
+            iconLeft={<Icon.User />}
             className="pl-4"
             placeholder="Type..."
           />
         </Input.Root>
         <Input.Root error className="relative max-w-[23.5rem]" fieldText="Name">
-          <Input.Input iconRight={InputError} placeholder="Type..." />
+          <Input.Text iconRight={<InputIcon.Error />} placeholder="Type..." />
         </Input.Root>
         <Input.Root fieldText="Choose an option" className="max-w-[23.5rem]">
-          <Input.SelectInput>
-            {options.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </Input.SelectInput>
+          <Input.SelectInput
+            options={[
+              "Select item 1",
+              "Select item 2",
+              "Select item 3",
+              "Select item 4",
+            ]}
+          />
+        </Input.Root>
+        <Input.Root className="max-w-[23.5rem]">
+          <Input.SelectInput
+            text="Select an option"
+            iconLeft={<Icon.User />}
+            options={[
+              "Select item 1",
+              "Select item 2",
+              "Select item 3",
+              "Select item 4",
+            ]}
+          />
         </Input.Root>
       </main>
     </HydrateClient>
