aad52aafdc85d62f859bcc9654a7795336d809ca
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-24 14:12:22 -0300

    💄 Style: rewrite default screens at tailwwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    extend: {
+      screens: {
+        "2xl": "87.5rem",
+        "3xl": "100rem",
+      },
+    },
     fontSize: {
       xs: "0.75rem",
       sm: "0.875rem",
