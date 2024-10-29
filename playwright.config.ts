6dff61ee1be00c56a5d44c979a2028a1d92f18db
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-29 16:14:38 -0300

    💚 Ci: add devices browsers at config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./src/e2e",
@@ -23,21 +23,21 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
   },
 
-  // projects: [
-  //   {
-  //     name: "chromium",
-  //     use: { ...devices["Desktop Chrome"] },
-  //   },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
 
-  //   {
-  //     name: "firefox",
-  //     use: { ...devices["Desktop Firefox"] },
-  //   },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
-  //   {
-  //     name: "webkit",
-  //     use: { ...devices["Desktop Safari"] },
-  //   },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
 
   /* Test against mobile viewports. */
   // {
@@ -58,5 +58,5 @@ export default defineConfig({
   //   name: 'Google Chrome',
   //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
   // },
-  // ],
+  ],
 });
