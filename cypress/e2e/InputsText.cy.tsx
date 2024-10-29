d67d00149b79c2f824e73840fddf7f7922e9b46f
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-29 14:48:39 -0300

    ➖ Remove: choose to not use cypress for e2e test

--- cypress/e2e/InputsText.cy.tsx
+++
@@ -1,8 +0,0 @@
-import { Input } from "~/common/ui/Input";
-
-describe("InputsText Te", () => {
-  it("Should render the input text", () => {
-    cy.visit("http://localhost:3000");
-    cy.get('h1').should("have.text", "Text"); 
-  });
-});
