f4268b345550b485cf3534ccb23967e59118204e
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-28 21:55:07 -0300

    🧑‍💻 Feat: add clerk ckeys to env.js

--- src/env.js
+++ src/env.js
@@ -8,7 +8,6 @@ export const env = createEnv({
 	 */
 	server: {
 		DATABASE_URL: z.string().url(),
-		CLERK_PUBLISHABLE_KEY: z.string(),
 		CLERK_SECRET_KEY: z.string(),
 		NODE_ENV: z
 			.enum(['development', 'test', 'production'])
@@ -21,7 +20,7 @@ export const env = createEnv({
 	 * `NEXT_PUBLIC_`.
 	 */
 	client: {
-		// NEXT_PUBLIC_CLIENTVAR: z.string(),
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string()
 	},
 
 	/**
@@ -31,9 +30,8 @@ export const env = createEnv({
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
-		CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-		// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 	},
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
