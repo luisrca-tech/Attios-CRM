d389dfed21867e2d9a48c8f222dc6ca750c410ee
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-28 22:10:36 -0300

    ✨ Feat: use email verification function and add submit type to button

--- src/feature/auth/components/SignUpEmailVerify.tsx
+++ src/feature/auth/components/SignUpEmailVerify.tsx
@@ -1,25 +1,38 @@
+"use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "~/common/ui/Button";
 import { Input } from "~/common/ui/Input";
+import { useAuth } from "../hook/useAuth";
 import { signUpEmailVerifySchema } from "../schemas/signUpEmailVerify.schema";
 import { type SignUpEmailVerify } from "../types/signUpEmailVerify.type";
 
 export function SignUpEmailVerify() {
-  const { register } = useForm<SignUpEmailVerify>({
+  const { register, handleSubmit } = useForm<SignUpEmailVerify>({
     resolver: zodResolver(signUpEmailVerifySchema),
     mode: "onChange",
   });
+  const { verifyEmail, resendCode } = useAuth();
+
+  async function handleConfirmCode({ code }: SignUpEmailVerify) {
+    await verifyEmail({ code });
+  }
 
   return (
-    <form className="w-[22.8125rem] flex flex-col gap-4">
-      <Input.Root fieldText="Password">
+    <form
+      onSubmit={handleSubmit(handleConfirmCode)}
+      className="w-[22.8125rem] flex flex-col gap-4"
+    >
+      <Input.Root fieldText="Code">
         <Input.Password placeholder="********" {...register("code")} />
       </Input.Root>
       <div className="flex gap-2 mt-3">
-        <Button className="w-full">Resend code</Button>
-        <Button color="secondary" className="w-full">
+        <Button type="submit" className="w-full">
           Confirm
+        </Button>
+        <Button className="w-full" color="secondary" onClick={resendCode}>
+          Resend code
         </Button>
       </div>
     </form>
