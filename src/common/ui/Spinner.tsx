2eda2ed6930b51b3f2a35577f5c9cae062a7487f
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-29 07:33:08 -0300

    ♻️ Refactor: change heigth and width at spinner

--- src/common/ui/Spinner.tsx
+++ src/common/ui/Spinner.tsx
@@ -1,3 +1,3 @@
 export function Spinner() {
-  return <div className="h-6 w-6 animate-spin rounded-full border-b-2 border-t-2 border-white-100"></div>;
+  return <div className="h-4 w-4 animate-spin rounded-full border-b-2 border-t-2 border-white-100"></div>;
 }
