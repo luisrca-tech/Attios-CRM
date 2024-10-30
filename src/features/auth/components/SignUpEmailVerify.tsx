0a8a9f802ddfe4917eb5b4ecf08f1c33bc0eff5e
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-30 15:51:10 -0300

    ♻️ Refactor: change password input to otp input

--- src/features/auth/components/SignUpEmailVerify.tsx
+++ src/features/auth/components/SignUpEmailVerify.tsx
@@ -3,13 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "~/common/ui/Button";
-import { Input } from "~/common/ui/Input";
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "~/common/ui/input-otp";
 import { useAuth } from "../hook/useAuth";
 import { signUpEmailVerifySchema } from "../schemas/signUpEmailVerify.schema";
 import { type SignUpEmailVerify } from "../types/signUpEmailVerify.type";
 
 export function SignUpEmailVerify() {
-  const { register, handleSubmit } = useForm<SignUpEmailVerify>({
+  const { setValue, handleSubmit } = useForm<SignUpEmailVerify>({
     resolver: zodResolver(signUpEmailVerifySchema),
     mode: "onChange",
   });
@@ -24,9 +24,21 @@ export function SignUpEmailVerify() {
       onSubmit={handleSubmit(handleConfirmCode)}
       className="w-[22.8125rem] flex flex-col gap-4"
     >
-      <Input.Root fieldText="Code">
-        <Input.Password placeholder="********" {...register("code")} />
-      </Input.Root>
+      <div className="flex justify-center items-center pb-4">
+        <InputOTP maxLength={6} onChange={(value) => setValue("code", value)}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+      </div>
       <div className="flex gap-2 mt-3">
         <Button type="submit" className="w-full" isLoading={isLoading}>
           Confirm
