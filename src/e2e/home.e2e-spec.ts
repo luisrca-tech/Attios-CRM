11aca51c2aee5ead030986b97aaf1f1a53dc35fb
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-02 12:53:35 -0300

    🔥 Remove: delete primary test home

--- src/e2e/home.e2e-spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("should show the text input", async ({ page }) => {
-  await page.goto("/", { waitUntil: "networkidle" });
-  await expect(page.getByRole("heading", { name: "Text" })).toBeVisible();
-});
