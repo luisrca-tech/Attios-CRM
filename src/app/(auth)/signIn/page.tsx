5276d3b0459b31a473f5ec89485d697ab5a58b4a
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-05 15:42:26 -0300

    ✨ Feat: make the signIn form

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,7 +1,14 @@
+import { AuthBackground } from '~/features/auth/components/AuthBackground';
+import signUpImage from '/public/images/signUpImage.png';
+import { SignInForm } from '~/features/auth/block/SignInForm';
+
 export default function SignIn() {
-  return (
-    <main className="flex flex-col gap-1">
-      <h1>signIn</h1>
-    </main>
-  );
+	return (
+		<main className="flex max-h-screen items-center overflow-hidden">
+			<div className="flex-1">
+				<SignInForm />
+			</div>
+			<AuthBackground image={signUpImage.src} />
+		</main>
+	);
 }
---
+++ src/features/auth/block/SignInForm.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button, LinkButton } from '~/common/components/ui/Button';
+import ErrorMessage from '~/common/components/ui/ErrorMessage';
+import { Icon } from '~/common/components/ui/Icons';
+import { Input } from '~/common/components/ui/Input';
+import { SocialAuth } from '../components/SocialAuth';
+import { WelcomeHeading } from '../components/WelcomeHeading';
+import { useAuth } from '../hook/useAuth';
+import { signInFormSchema } from '../schemas/signInForm.schema';
+import type { SignInFormType } from '../types/signInForm.type';
+
+export function SignInForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitSuccessful, isSubmitted }
+	} = useForm<SignInFormType>({
+		resolver: zodResolver(signInFormSchema),
+		mode: 'onChange'
+	});
+	const { isSignUpLoaded } = useAuth();
+
+	if (!isSignUpLoaded) return null;
+
+	async function onSubmit({ email, password }: SignInFormType) {
+		console.log(email, password);
+	}
+
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<WelcomeHeading
+				title="Welcome to our CRM. Sign In to see latest updates."
+				subtitle="Enter your details to proceed further"
+			/>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="flex w-[22.8125rem] flex-col gap-4"
+			>
+				<Input.Root
+					error={!!errors.email?.message}
+					isValid={isSubmitSuccessful}
+					fieldText="Email"
+				>
+					<Input.Text
+						placeholder="Johndoe@example.com"
+						renderIconRight={() => <Icon.Email />}
+						{...register('email')}
+					/>
+				</Input.Root>
+				{errors.email?.message && isSubmitted && (
+					<ErrorMessage>{errors.email.message}</ErrorMessage>
+				)}
+				<Input.Root
+					error={!!errors.password?.message}
+					isValid={isSubmitSuccessful}
+					fieldText="Password"
+				>
+					<Input.Password
+						placeholder="********"
+						{...register('password')}
+						className="h-full w-full p-2 font-bold text-black text-sm leading-5 placeholder:text-primary-200 focus:border-white-400 focus:outline-white-400"
+					/>
+				</Input.Root>
+				{errors.password?.message && isSubmitted && (
+					<ErrorMessage>
+						<p>{errors.password.message}</p>
+					</ErrorMessage>
+				)}
+				<button
+					className='text-right font-bold text-primary-100 text-sm leading-4 hover:opacity-70'
+					type="button"
+				>
+					Recover password
+				</button>
+				<div className="mt-3 flex gap-2">
+					<Button type="submit" className="w-full">
+						Sign In
+					</Button>
+					<LinkButton href="/signUp" color="secondary" className="w-full">
+						Sign Up
+					</LinkButton>
+				</div>
+			</form>
+			<SocialAuth />
+		</div>
+	);
+}
