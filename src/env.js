556f60e3a3ab05b2b026c4c19c0901045357cd69
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-06 18:27:32 -0300

    🔧 Chore: change clerk  api to server envroiments

--- src/env.js
+++ src/env.js
@@ -9,6 +9,7 @@ export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().url(),
 		CLERK_SECRET_KEY: z.string(),
+		CLERK_FRONTEND_API: z.string(),
 		NODE_ENV: z
 			.enum(['development', 'test', 'production'])
 			.default('development'),
@@ -22,7 +23,6 @@ export const env = createEnv({
 	 */
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-		NEXT_PUBLIC_CLERK_FRONTEND_API: z.string()
 	},
 
 	/**
@@ -33,9 +33,9 @@ export const env = createEnv({
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-		NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
 		WEBHOOK_SECRET: process.env.WEBHOOK_SECRET
 	},
 	/**
