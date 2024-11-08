13c245a6fe1b3d893b030d5947b75d65b45143d5
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-08 14:27:51 -0300

    🔥 Remove: delete sidebar

--- src/common/components/block/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-export function Sidebar() {
-	return (
-		<div className="flex h-full w-full flex-col bg-background-secondary">
-			<div className="flex h-16 w-full items-center justify-between px-4">
-				<h1>Dashboard</h1>
-			</div>
-		</div>
-	);
-}
