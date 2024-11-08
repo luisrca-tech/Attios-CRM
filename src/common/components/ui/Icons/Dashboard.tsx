b3e7ae0548619d1b410e35a23490ab38c48f73b2
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-08 14:29:09 -0300

    💄 Style: update styles

--- src/common/components/ui/Icons/Dashboard.tsx
+++ src/common/components/ui/Icons/Dashboard.tsx
@@ -1,10 +1,14 @@
-import Image from 'next/image';
-import type { IconProps } from '~/common/types/Icons.type';
-import { cn } from '~/lib/utils';
-import DashboardIconImage from '/public/icons/sidebar/dashboard.png';
+	import Image from 'next/image';
+	import type { IconProps } from '~/common/types/Icons.type';
+	import { cn } from '~/lib/utils';
+	import DashboardIconImage from '/public/icons/sidebar/dashboard.png';
 
-export function DashboardIcon({ className }: IconProps) {
-	return (
-		<Image className={cn(className)} src={DashboardIconImage} alt="Dashboard" />
-	);
-}
+	export function DashboardIcon({ className }: IconProps) {
+		return (
+			<Image
+				className={cn('text-primary-100 ', className)}
+				src={DashboardIconImage}
+				alt="Dashboard"
+			/>
+		);
+	}
