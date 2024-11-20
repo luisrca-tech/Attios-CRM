edb0c53908450f11e71b8331fe111f11cdada22a
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-20 13:37:00 -0300

    📱 Refactor: Add an indicator to see the botoom menu

--- src/stories/BottomMenuNavigation.stories.tsx
+++ src/stories/BottomMenuNavigation.stories.tsx
@@ -5,19 +5,12 @@ import { ClerkProvider } from '@clerk/nextjs';
 const meta = {
   title: 'Navigation/BottomMenu',
   component: BottomMenu,
-  parameters: {
-    layout: 'fullscreen',
-    viewport: {
-      defaultViewport: 'mobile2',
-    },
-  },
   decorators: [
     (Story) => (
       <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? ''}>
         <div className="h-screen bg-white-100">
           <div className="p-4">
-            <h1 className="text-2xl font-bold">Content Area</h1>
-            <p>The bottom navigation bar is shown below</p>
+            <h1 className="text-2xl font-bold">Please, place the screen on mobile to see the bottom menu</h1>
           </div>
           <Story />
         </div>
