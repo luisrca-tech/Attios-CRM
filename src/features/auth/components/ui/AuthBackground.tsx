194c05accb559b8f21627e6b14678703d4a8f607
Luis Felipe <luisrochacruzalves@gmail.com>
2024-12-06 10:54:25 -0300

    🔍️ Seo: use next image

--- src/features/auth/components/ui/AuthBackground.tsx
+++ src/features/auth/components/ui/AuthBackground.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type AuthBackgroundProps = {
 	image: string;
 };
@@ -6,7 +8,7 @@ export function AuthBackground({ image }: AuthBackgroundProps) {
 	return (
 		<div className="hidden min-h-screen 3xl:max-w-full items-center justify-center bg-gradient-to-r from-primary-100 to-[#1B51E5] lg:flex lg:max-w-[34rem] 2xl:max-w-[40.875rem]">
 			<div className="z-10">
-				<img src={image} alt="Auth background" />
+				<Image src={image} alt="Auth background" width={800} height={800} />
 			</div>
 		</div>
 	);
