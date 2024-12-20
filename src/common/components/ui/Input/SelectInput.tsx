641d37e655cffdb1ecb9708a7893b8f83b43543d
Luis Felipe <luisrochacruzalves@gmail.com>
2024-12-20 12:02:57 -0300

    ♻️ Refactor: improve name of state and rotate arrow

--- src/common/components/ui/Input/SelectInput.tsx
+++ src/common/components/ui/Input/SelectInput.tsx
@@ -18,13 +18,18 @@ const SelectInput: React.FC<SelectInputProps> = ({
 	renderIcon,
 	renderOptionIcon
 }) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpenDropdown, setIsOpenDropdown] = useState(false);
 	const [selectedOption, setSelectedOption] = useState<string | null>(null);
 	const ref = useRef<HTMLDivElement>(null);
 
-	const toggleDropdown = () => setIsOpen(!isOpen);
+	const toggleDropdown = () => setIsOpenDropdown((prev) => !prev);
 
-	useClickOutside(ref, () => setIsOpen(false));
+	useClickOutside(ref, () => setIsOpenDropdown(false));
+	
+	const handleOptionClick = (option: string) => {
+		setSelectedOption(option);
+		toggleDropdown()
+	};
 
 	return (
 		<div
@@ -32,31 +37,30 @@ const SelectInput: React.FC<SelectInputProps> = ({
 			className="relative inline-block w-full cursor-pointer text-left"
 		>
 			<button
-				onClick={toggleDropdown}
+			onClick={toggleDropdown}
 				className={cn(
 					'flex w-full items-center justify-between rounded border-none font-bold text-sm leading-5 outline-none',
 					'placeholder:text-primary-200 hover:text-primary-100'
 				)}
 				type="button"
 			>
-				<span className="ml-2">{selectedOption || text}</span>
-				{renderIcon?.(isOpen)}
+				<span className="py-[0.375rem] text-primary-200 font-bold text-sm leading-5">{selectedOption || text}</span>
+				<div className={cn('transition-all duration-300', isOpenDropdown ? 'rotate-180' : '')}>
+				{renderIcon?.(isOpenDropdown)}
+				</div>
 			</button>
-			{isOpen && (
-				<div className="absolute mt-2 w-full rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+			{isOpenDropdown && (
+				<div className="absolute mt-2 w-full rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 bg-white-100">
 					<div className="py-1">
 						{options.map((option) => (
 							<button
 								key={option}
 								value={option}
 								className={cn(
-									'flex w-full items-center px-4 py-5 text-left text-sm',
+									'flex w-full items-center px-4 py-5 text-left text-sm hover:bg-primary-100/10',
 									'hover:text-primary-100'
 								)}
-								onClick={() => {
-									setSelectedOption(option);
-									setIsOpen(false);
-								}}
+								onClick={() => handleOptionClick(option)}
 								type="button"
 							>
 								{renderOptionIcon?.(option)}
