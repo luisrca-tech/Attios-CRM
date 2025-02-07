4e36f07f9caee2a2c1b73835e9f0a61f5a310a84
Luis Felipe <luisrochacruzalves@gmail.com>
2025-02-07 09:43:24 -0300

    ✅ Test: make the E2E to create product successfully with playwright

--- src/e2e/authenticated.e2e-spec.ts
+++ src/e2e/authenticated.e2e-spec.ts
@@ -1,6 +1,30 @@
 import { setupClerkTestingToken } from '@clerk/testing/playwright';
 import { expect, test } from '@playwright/test';
 
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' });
+
+  await page.getByLabel('Full Name').fill('John Doe');
+  await page.getByLabel('Email').fill('john+clerk_test@example.com');
+  await page.getByLabel('Password').fill('Jd16012003@');
+
+  await page.getByRole('button', { name: 'Sign Up' }).click();
+
+  const toast = page.getByText(
+    'We sent you a verification code to your email.'
+  );
+  await expect(toast).toBeVisible();
+
+  await page
+    .getByRole('textbox', { name: 'Enter the code we sent to your email' })
+    .fill('424242');
+  await page.getByRole('button', { name: 'Confirm' }).click();
+
+  await page.waitForURL('/', { waitUntil: 'networkidle' });
+  const userButton = page.getByRole('button', { name: /user/i });
+  await expect(userButton).toBeVisible();
+});
+
 test('sign in successfully', async ({ page }) => {
 	await page.goto('/sign-in', { waitUntil: 'networkidle' });
 
@@ -14,29 +38,6 @@ test('sign in successfully', async ({ page }) => {
 	await expect(userButton).toBeVisible();
 });
 
-test('sign up successfully', async ({ page }) => {
-	await page.goto('/sign-up', { waitUntil: 'networkidle' });
-
-	await page.getByLabel('Full Name').fill('John Doe');
-	await page.getByLabel('Email').fill('john+clerk_test@example.com');
-	await page.getByLabel('Password').fill('Jd16012003@');
-
-	await page.getByRole('button', { name: 'Sign Up' }).click();
-
-	const toast = page.getByText(
-		'We sent you a verification code to your email.'
-	);
-	await expect(toast).toBeVisible();
-
-	await page
-		.getByRole('textbox', { name: 'Enter the code we sent to your email' })
-		.fill('424242');
-	await page.getByRole('button', { name: 'Confirm' }).click();
-
-	await page.waitForURL('/', { waitUntil: 'networkidle' });
-	const userButton = page.getByRole('button', { name: /user/i });
-	await expect(userButton).toBeVisible();
-});
 
 test('resend code', async ({ page }) => {
 	await setupClerkTestingToken({ page });
@@ -49,10 +50,7 @@ test('resend code', async ({ page }) => {
 
 	await page.getByRole('button', { name: 'Sign Up' }).click();
 
-	const toast = page.getByText(
-		'We sent you a verification code to your email.'
-	);
-	await expect(toast).toBeVisible();
+	await expect(page.getByText('We sent you a verification code to your email.')).toBeVisible();
 
 	await page.getByRole('button', { name: 'Resend Code' }).click();
 
