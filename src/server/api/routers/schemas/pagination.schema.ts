af436fcf358905772c8eb07b49abd560df5d0692
Luis Felipe <luisrochacruzalves@gmail.com>
2025-03-14 15:41:19 -0300

    🚨 Fix: run linter

--- src/server/api/routers/schemas/pagination.schema.ts
+++ src/server/api/routers/schemas/pagination.schema.ts
@@ -1,7 +1,7 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const paginationSchema = z.object({
-  page: z.number().min(1).default(1),
-  pageSize: z.number().min(1).max(50).default(10),
-  search: z.string().optional(),
+	page: z.number().min(1).default(1),
+	pageSize: z.number().min(1).max(50).default(10),
+	search: z.string().optional()
 });
--- src/server/api/routers/schemas/totalPagesQuery.schema.ts
+++ src/server/api/routers/schemas/totalPagesQuery.schema.ts
@@ -1,6 +1,6 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const totalPagesQuerySchema = z.object({
-  pageSize: z.number().min(1).max(50).default(10),
-  search: z.string().optional(),
+	pageSize: z.number().min(1).max(50).default(10),
+	search: z.string().optional()
 });
