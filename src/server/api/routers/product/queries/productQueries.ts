34e981650bd7cf39698057a0ed00b3ff0ab9df00
Luis Felipe <luisrochacruzalves@gmail.com>
2025-03-25 11:24:23 -0300

    🚸 Feat: adapt the products infinite query to be sorted by name

--- src/server/api/routers/product/queries/productQueries.ts
+++ src/server/api/routers/product/queries/productQueries.ts
@@ -1,85 +1,91 @@
-import { eq, sql, asc, desc } from 'drizzle-orm';
-import { z } from 'zod';
-import { publicProcedure } from '~/server/api/trpc';
-import { products } from '~/server/db/schema';
-import { controlledQuerySchema } from '../../schemas/controlledQuery.schema';
-import { totalPagesQuerySchema } from '../../schemas/totalPagesQuery.schema';
-import { paginatedProductsSchema } from '../schemas/paginatedProducts.schema';
+import { eq, sql, asc, desc } from "drizzle-orm";
+import { z } from "zod";
+import { publicProcedure } from "~/server/api/trpc";
+import { products } from "~/server/db/schema";
+import { totalPagesQuerySchema } from "../../schemas/totalPagesQuery.schema";
+import { paginatedProductsSchema } from "../schemas/paginatedProducts.schema";
+import { controlledProductsSchema } from "../schemas/controlledProducts.schema";
 
 const requiredProductRelations = {
-	category: {
-		columns: {
-			id: true,
-			name: true
-		}
-	},
-	productImages: {
-		columns: {
-			url: true,
-			key: true
-		}
-	}
+  category: {
+    columns: {
+      id: true,
+      name: true,
+    },
+  },
+  productImages: {
+    columns: {
+      url: true,
+      key: true,
+    },
+  },
 } as const;
 
 export const productQueries = {
-	// This is a paginated query that returns a list of products to be displayed in the desktop product tablee
-	getProductsPaginated: publicProcedure
-		.input(paginatedProductsSchema)
-		.query(async ({ ctx, input }) => {
-			return ctx.db.query.products.findMany({
-				with: requiredProductRelations,
-				limit: input.pageSize,
-				offset: (input.page - 1) * input.pageSize,
-				orderBy: input.sort
-					? [
-							input.sort.direction === 'asc'
-								? asc(products[input.sort.column])
-								: desc(products[input.sort.column])
-						]
-					: [asc(products.name)]
-			});
-		}),
+  // This is a paginated query that returns a list of products to be displayed in the desktop product tablee
+  getProductsPaginated: publicProcedure
+    .input(paginatedProductsSchema)
+    .query(async ({ ctx, input }) => {
+      return ctx.db.query.products.findMany({
+        with: requiredProductRelations,
+        limit: input.pageSize,
+        offset: (input.page - 1) * input.pageSize,
+        orderBy: input.sort
+          ? [
+              input.sort.direction === "asc"
+                ? asc(products[input.sort.column])
+                : desc(products[input.sort.column]),
+            ]
+          : [asc(products.name)],
+      });
+    }),
 
-	getTotalPages: publicProcedure
-		.input(totalPagesQuerySchema)
-		.query(async ({ ctx, input }) => {
-			const [result] = await ctx.db
-				.select({ count: sql<number>`count(*)`.mapWith(Number) })
-				.from(products);
+  getTotalPages: publicProcedure
+    .input(totalPagesQuerySchema)
+    .query(async ({ ctx, input }) => {
+      const [result] = await ctx.db
+        .select({ count: sql<number>`count(*)`.mapWith(Number) })
+        .from(products);
 
-			const totalCount = result?.count ?? 0;
-			return Math.ceil(totalCount / input.pageSize);
-		}),
+      const totalCount = result?.count ?? 0;
+      return Math.ceil(totalCount / input.pageSize);
+    }),
 
-	// This is the infinite scroll query for the mobile product table
-	getControlledProductsInfinite: publicProcedure
-		.input(controlledQuerySchema)
-		.query(async ({ ctx, input }) => {
-			const { limit, cursor } = input;
+  // This is the infinite scroll query for the mobile product table
+  getControlledProductsInfinite: publicProcedure
+    .input(controlledProductsSchema)
+    .query(async ({ ctx, input }) => {
+      const { limit, cursor, sort } = input;
 
-			const items = await ctx.db.query.products.findMany({
-				with: requiredProductRelations,
-				limit: limit + 1,
-				offset: cursor,
-				orderBy: [asc(products.name)]
-			});
+      const items = await ctx.db.query.products.findMany({
+        with: requiredProductRelations,
+        limit: limit + 1,
+        offset: cursor,
+        orderBy: sort
+          ? [
+              sort.direction === "asc"
+                ? asc(products[sort.column])
+                : desc(products[sort.column]),
+            ]
+          : [asc(products.name)],
+      });
 
-			let nextCursor: number | undefined = undefined;
-			if (items.length > limit) {
-				items.pop();
-				nextCursor = cursor + limit;
-			}
+      let nextCursor: number | undefined = undefined;
+      if (items.length > limit) {
+        items.pop();
+        nextCursor = cursor + limit;
+      }
 
-			return {
-				items,
-				nextCursor
-			};
-		}),
+      return {
+        items,
+        nextCursor,
+      };
+    }),
 
-	getById: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-		return ctx.db.query.products.findFirst({
-			where: eq(products.id, input),
-			with: requiredProductRelations
-		});
-	})
+  getById: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.db.query.products.findFirst({
+      where: eq(products.id, input),
+      with: requiredProductRelations,
+    });
+  }),
 };
