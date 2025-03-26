64710c1d3d59c563b1a11ff85930d3541b787454
Luis Felipe <luisrochacruzalves@gmail.com>
2025-03-26 09:56:50 -0300

    🚨 Fix: run linter

--- src/common/components/ui/DeleteConfirmationModal.tsx
+++ src/common/components/ui/DeleteConfirmationModal.tsx
@@ -1,51 +1,40 @@
-import { Button } from "./Button";
+import { Button } from './Button';
 import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "./dialog";
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle
+} from './dialog';
 
 interface DeleteConfirmationModalProps {
-  onConfirm: () => void;
-  onCancel: () => void;
-  isOpen: boolean;
+	onConfirm: () => void;
+	onCancel: () => void;
+	isOpen: boolean;
 }
 
 export const DeleteConfirmationModal = ({
-  onConfirm,
-  onCancel,
-  isOpen,
+	onConfirm,
+	onCancel,
+	isOpen
 }: DeleteConfirmationModalProps) => {
-  return (
-    <Dialog
-      open={isOpen}
-      onOpenChange={onCancel}
-    >
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="font-bold text-base text-black">
-            Are you sure you want to delete this item?
-          </DialogTitle>
-        </DialogHeader>
-        <DialogFooter>
-          <Button
-            type="button"
-            color="quaternary"
-            onClick={onConfirm}
-          >
-            Confirm
-          </Button>
-          <Button
-            type="button"
-            color="secondary"
-            onClick={onCancel}
-          >
-            Cancel
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+	return (
+		<Dialog open={isOpen} onOpenChange={onCancel}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className="font-bold text-base text-black">
+						Are you sure you want to delete this item?
+					</DialogTitle>
+				</DialogHeader>
+				<DialogFooter>
+					<Button type="button" color="quaternary" onClick={onConfirm}>
+						Confirm
+					</Button>
+					<Button type="button" color="secondary" onClick={onCancel}>
+						Cancel
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
 };
