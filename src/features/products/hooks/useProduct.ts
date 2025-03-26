7221c140360ea1042c7c5ae3aad849855c33b60c
Luis Felipe <luisrochacruzalves@gmail.com>
2025-03-26 09:55:36 -0300

    ✨ Feat: create the deletion logic to products

--- src/features/products/hooks/useProduct.ts
+++ src/features/products/hooks/useProduct.ts
@@ -1,104 +1,115 @@
-import { useState } from 'react';
-import { toast } from 'sonner';
-import { api } from '~/trpc/react';
+import { useState } from "react";
+import { toast } from "sonner";
+import { api } from "~/trpc/react";
 
 export const useProduct = () => {
-	const [categorySearch, setCategorySearch] = useState('');
-	const [brandSearch, setBrandSearch] = useState('');
+  const [categorySearch, setCategorySearch] = useState("");
+  const [brandSearch, setBrandSearch] = useState("");
 
-	const getAllCategories = api.category.getAll.useQuery();
-	const getAllBrands = api.brand.getAll.useQuery();
-	const categories = getAllCategories.data?.map((cat) => cat.name) ?? [];
-	const brands = getAllBrands.data?.map((brand) => brand.name) ?? [];
+  const getAllCategories = api.category.getAll.useQuery();
+  const getAllBrands = api.brand.getAll.useQuery();
+  const categories = getAllCategories.data?.map((cat) => cat.name) ?? [];
+  const brands = getAllBrands.data?.map((brand) => brand.name) ?? [];
 
-	const filteredCategories = categories.filter((category) =>
-		category.toLowerCase().includes(categorySearch.toLowerCase())
-	);
+  const filteredCategories = categories.filter((category) =>
+    category.toLowerCase().includes(categorySearch.toLowerCase())
+  );
 
-	const filteredBrands = brands.filter((brand) =>
-		brand.toLowerCase().includes(brandSearch.toLowerCase())
-	);
+  const filteredBrands = brands.filter((brand) =>
+    brand.toLowerCase().includes(brandSearch.toLowerCase())
+  );
 
-	const trpcUtils = api.useUtils();
+  const trpcUtils = api.useUtils();
 
-	const createCategory = api.category.create.useMutation({
-		onMutate: async (data) => {
-			const newCategory = {
-				id: Math.random(),
-				name: data.name,
-				products: []
-			};
-			await trpcUtils.category.getAll.cancel();
+  const createCategory = api.category.create.useMutation({
+    onMutate: async (data) => {
+      const newCategory = {
+        id: Math.random(),
+        name: data.name,
+        products: [],
+      };
+      await trpcUtils.category.getAll.cancel();
 
-			trpcUtils.category.getAll.setData(undefined, (old) => {
-				if (!old) return [newCategory];
-				return [...old, newCategory];
-			});
+      trpcUtils.category.getAll.setData(undefined, (old) => {
+        if (!old) return [newCategory];
+        return [...old, newCategory];
+      });
 
-			return { newCategory };
-		},
-		onError: (_err, _variables, ctx) => {
-			toast.error('Something went wrong');
-			trpcUtils.category.getAll.setData(
-				undefined,
-				ctx?.newCategory ? [ctx.newCategory] : []
-			);
-		},
-		onSettled: async () => {
-			await Promise.all([trpcUtils.category.getAll.invalidate()]);
-			return toast.success('Category added');
-		}
-	});
+      return { newCategory };
+    },
+    onError: (_err, _variables, ctx) => {
+      toast.error("Something went wrong");
+      trpcUtils.category.getAll.setData(
+        undefined,
+        ctx?.newCategory ? [ctx.newCategory] : []
+      );
+    },
+    onSettled: async () => {
+      await Promise.all([trpcUtils.category.getAll.invalidate()]);
+      return toast.success("Category added");
+    },
+  });
 
-	const createBrand = api.brand.create.useMutation({
-		onMutate: async (data) => {
-			const newBrand = {
-				id: Math.random(),
-				name: data.name,
-				products: []
-			};
-			await trpcUtils.brand.getAll.cancel();
+  const createBrand = api.brand.create.useMutation({
+    onMutate: async (data) => {
+      const newBrand = {
+        id: Math.random(),
+        name: data.name,
+        products: [],
+      };
+      await trpcUtils.brand.getAll.cancel();
 
-			trpcUtils.brand.getAll.setData(undefined, (old) => {
-				if (!old) return [newBrand];
-				return [...old, newBrand];
-			});
+      trpcUtils.brand.getAll.setData(undefined, (old) => {
+        if (!old) return [newBrand];
+        return [...old, newBrand];
+      });
 
-			return { newBrand };
-		},
-		onError: (_err, _variables, ctx) => {
-			toast.error('Something went wrong');
-			trpcUtils.brand.getAll.setData(
-				undefined,
-				ctx?.newBrand ? [ctx.newBrand] : []
-			);
-		},
-		onSettled: async () => {
-			await Promise.all([trpcUtils.brand.getAll.invalidate()]);
-			return toast.success('Brand added');
-		}
-	});
+      return { newBrand };
+    },
+    onError: (_err, _variables, ctx) => {
+      toast.error("Something went wrong");
+      trpcUtils.brand.getAll.setData(
+        undefined,
+        ctx?.newBrand ? [ctx.newBrand] : []
+      );
+    },
+    onSettled: async () => {
+      await Promise.all([trpcUtils.brand.getAll.invalidate()]);
+      return toast.success("Brand added");
+    },
+  });
 
-	const createProduct = api.product.create.useMutation({
-		onError: (error) => {
-			toast.error(error.message);
-		}
-	});
+  const createProduct = api.product.create.useMutation({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-	const updateProduct = api.product.update.useMutation({
-		onError: (error) => {
-			toast.error(error.message);
-		}
-	});
+  const updateProduct = api.product.update.useMutation({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-	return {
-		createCategory,
-		createProduct,
-		createBrand,
-		filteredCategories,
-		filteredBrands,
-		setCategorySearch,
-		setBrandSearch,
-		updateProduct
-	};
+  const deleteProduct = api.product.delete.useMutation({
+    onSuccess: () => {
+      trpcUtils.product.getProductsPaginated.invalidate();
+      trpcUtils.product.getControlledProductsInfinite.invalidate();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return {
+    createCategory,
+    createProduct,
+    createBrand,
+    filteredCategories,
+    filteredBrands,
+    setCategorySearch,
+    setBrandSearch,
+    updateProduct,
+    deleteProduct,
+  };
 };
--- src/server/api/routers/product/mutations/productMutations.ts
+++ src/server/api/routers/product/mutations/productMutations.ts
@@ -1,86 +1,104 @@
-import { eq, sql } from 'drizzle-orm';
-import { randomUUID } from 'node:crypto';
-import { newProductSchema } from '~/features/products/schemas/newProduct.schema';
-import { updateProductSchema } from '~/features/products/schemas/updateProduct.schema';
-import { protectedProcedure } from '~/server/api/trpc';
-import { products, productImages } from '~/server/db/schema';
+import { eq, sql } from "drizzle-orm";
+import { randomUUID } from "node:crypto";
+import { z } from "zod";
+import { newProductSchema } from "~/features/products/schemas/newProduct.schema";
+import { updateProductSchema } from "~/features/products/schemas/updateProduct.schema";
+import { protectedProcedure } from "~/server/api/trpc";
+import { products, productImages, orderItems } from "~/server/db/schema";
 
 export const productMutations = {
-	create: protectedProcedure
-		.input(newProductSchema)
-		.mutation(async ({ ctx, input }) => {
-			return await ctx.db.transaction(async (tx) => {
-				const category = await tx.query.categories.findFirst({
-					where: (categories, { eq }) => eq(categories.name, input.category)
-				});
+  create: protectedProcedure
+    .input(newProductSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.transaction(async (tx) => {
+        const category = await tx.query.categories.findFirst({
+          where: (categories, { eq }) => eq(categories.name, input.category),
+        });
 
-				if (!category) {
-					throw new Error('Category not found');
-				}
+        if (!category) {
+          throw new Error("Category not found");
+        }
 
-				const brand = await tx.query.brands.findFirst({
-					where: (brands, { eq }) => eq(brands.name, input.brand)
-				});
+        const brand = await tx.query.brands.findFirst({
+          where: (brands, { eq }) => eq(brands.name, input.brand),
+        });
 
-				if (!brand) {
-					throw new Error('Brand not found');
-				}
+        if (!brand) {
+          throw new Error("Brand not found");
+        }
 
-				const productId = randomUUID().slice(0, 10);
+        const productId = randomUUID().slice(0, 10);
 
-				const product = await tx
-					.insert(products)
-					.values({
-						id: productId,
-						brandId: brand.id,
-						name: input.name,
-						sku: input.sku,
-						listPrice: sql`${input.price}::decimal`,
-						quantity: input.availableQuantity,
-						categoryId: category.id,
-						categoryName: category.name,
-						modelYear: new Date().getFullYear()
-					})
-					.returning({
-						id: products.id
-					});
+        const product = await tx
+          .insert(products)
+          .values({
+            id: productId,
+            brandId: brand.id,
+            name: input.name,
+            sku: input.sku,
+            listPrice: sql`${input.price}::decimal`,
+            quantity: input.availableQuantity,
+            categoryId: category.id,
+            categoryName: category.name,
+            modelYear: new Date().getFullYear(),
+          })
+          .returning({
+            id: products.id,
+          });
 
-				return product;
-			});
-		}),
+        return product;
+      });
+    }),
 
-	update: protectedProcedure
-		.input(updateProductSchema)
-		.mutation(async ({ ctx, input }) => {
-			return await ctx.db.transaction(async (tx) => {
-				await tx
-					.update(products)
-					.set({
-						name: input.name,
-						sku: input.sku,
-						listPrice: sql`${input.price}::decimal`,
-						quantity: input.availableQuantity,
-						categoryName: input.category,
-						subcategory: input.subcategory ?? null,
-						currency: input.currency
-					})
-					.where(eq(products.id, input.productId));
+  update: protectedProcedure
+    .input(updateProductSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.transaction(async (tx) => {
+        await tx
+          .update(products)
+          .set({
+            name: input.name,
+            sku: input.sku,
+            listPrice: sql`${input.price}::decimal`,
+            quantity: input.availableQuantity,
+            categoryName: input.category,
+            subcategory: input.subcategory ?? null,
+            currency: input.currency,
+          })
+          .where(eq(products.id, input.productId));
 
-				if (input.productImages && input.productImages.length > 0) {
-					await tx
-						.delete(productImages)
-						.where(eq(productImages.productId, input.productId));
+        if (input.productImages && input.productImages.length > 0) {
+          await tx
+            .delete(productImages)
+            .where(eq(productImages.productId, input.productId));
 
-					await tx.insert(productImages).values(
-						input.productImages.map((image) => ({
-							productId: input.productId,
-							url: image.url,
-							key: image.key
-						}))
-					);
-				}
+          await tx.insert(productImages).values(
+            input.productImages.map((image) => ({
+              productId: input.productId,
+              url: image.url,
+              key: image.key,
+            }))
+          );
+        }
 
-				return { id: input.productId };
-			});
-		})
+        return { id: input.productId };
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.transaction(async (tx) => {
+        // First delete all associated order items
+        await tx.delete(orderItems).where(eq(orderItems.productId, input.id));
+
+        // Then delete all associated images
+        await tx
+          .delete(productImages)
+          .where(eq(productImages.productId, input.id));
+
+        // Finally delete the product
+        return await tx.delete(products).where(eq(products.id, input.id));
+      });
+    }),
 };
