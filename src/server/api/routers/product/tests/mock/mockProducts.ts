aa676683928512947a238b2cabdf68c5c236a809
Luis Felipe <luisrochacruzalves@gmail.com>
2025-04-04 10:47:36 -0300

    🚨 Fix: run linter

--- src/server/api/routers/product/tests/mock/mockProducts.ts
+++ src/server/api/routers/product/tests/mock/mockProducts.ts
@@ -1,32 +1,32 @@
-import type { MockProduct } from "../types/mockProduct.type";
+import type { MockProduct } from '../types/mockProduct.type';
 
 export const mockProducts: MockProduct[] = [
-  {
-    id: "PROD1",
-    name: "Test Product 1",
-    brandId: 1,
-    categoryId: 1,
-    categoryName: null,
-    modelYear: 2024,
-    quantity: 10,
-    listPrice: "99.99",
-    sku: null,
-    currency: null,
-    subcategory: null,
-    description: null,
-  },
-  {
-    id: "PROD2",
-    name: "Test Product 2",
-    brandId: 1,
-    categoryId: 1,
-    categoryName: null,
-    modelYear: 2024,
-    quantity: 5,
-    listPrice: "149.99",
-    sku: null,
-    currency: null,
-    subcategory: null,
-    description: null,
-  },
+	{
+		id: 'PROD1',
+		name: 'Test Product 1',
+		brandId: 1,
+		categoryId: 1,
+		categoryName: null,
+		modelYear: 2024,
+		quantity: 10,
+		listPrice: '99.99',
+		sku: null,
+		currency: null,
+		subcategory: null,
+		description: null
+	},
+	{
+		id: 'PROD2',
+		name: 'Test Product 2',
+		brandId: 1,
+		categoryId: 1,
+		categoryName: null,
+		modelYear: 2024,
+		quantity: 5,
+		listPrice: '149.99',
+		sku: null,
+		currency: null,
+		subcategory: null,
+		description: null
+	}
 ];
