8af2e022ef7fa14cad00aac32bd72de6e59a0f2e
Luis Felipe <luisrochacruzalves@gmail.com>
2025-05-07 14:16:16 -0300

    ✨ Update: Standardize import quotes, enhance formatting, and refactor TeamsForm and useTeams hook for improved readability and consistency.

--- src/server/api/routers/teams/mutations/team.mutation.ts
+++ src/server/api/routers/teams/mutations/team.mutation.ts
@@ -1,57 +1,70 @@
-import { eq } from 'drizzle-orm';
-import { protectedProcedure } from '~/server/api/trpc';
-import { subDomains, teamUsers, teams, users } from '~/server/db/schema';
-import { createTeamSchema } from '../schemas/teams.schema';
+import { eq } from "drizzle-orm";
+import { protectedProcedure } from "~/server/api/trpc";
+import { subDomains, teamUsers, teams, users } from "~/server/db/schema";
+import { createTeamSchema } from "../schemas/teams.schema";
 
 export const teamMutations = {
-	create: protectedProcedure
-		.input(createTeamSchema)
-		.mutation(async ({ ctx, input }) => {
-			const { name, subdomain } = input;
+  create: protectedProcedure
+    .input(createTeamSchema)
+    .mutation(async ({ ctx, input }) => {
+      const { name, subdomain } = input;
 
-			const userHasSubDomain = await ctx.db.query.subDomains.findMany({
-				where: eq(subDomains.subDomain, subdomain)
-			});
+      const userHasSubDomain = await ctx.db.query.subDomains.findMany({
+        where: eq(subDomains.subDomain, subdomain),
+      });
 
-			if (userHasSubDomain.length > 0) {
-				console.log('User has subdomain');
-				const team = await ctx.db.insert(teams).values({ name }).returning();
-				return team;
-			}
+      if (userHasSubDomain.length > 0) {
+        console.log("User has subdomain");
+        const team = await ctx.db.transaction(async (tx) => {
+          const team = await tx.insert(teams).values({ name }).returning();
 
-			const team = await ctx.db.transaction(async (tx) => {
-				const team = await tx.insert(teams).values({ name }).returning();
+          if (!team[0]) {
+            throw new Error("Team not created");
+          }
 
-				if (!team[0]) {
-					throw new Error('Team not created');
-				}
+          await tx.insert(teamUsers).values({
+            teamId: team[0].id,
+            userId: ctx.session.userId,
+          });
 
-				const subDomain = await tx
-					.insert(subDomains)
-					.values({
-						subDomain: subdomain
-					})
-					.returning();
+          return team;
+        });
+        return team;
+      }
 
-				if (!subDomain[0]) {
-					throw new Error('Subdomain not created');
-				}
+      const team = await ctx.db.transaction(async (tx) => {
+        const team = await tx.insert(teams).values({ name }).returning();
 
-				await tx
-					.update(users)
-					.set({
-						subDomainId: subDomain[0].id
-					})
-					.where(eq(users.id, ctx.session.userId));
+        if (!team[0]) {
+          throw new Error("Team not created");
+        }
 
-				await tx.insert(teamUsers).values({
-					teamId: team[0].id,
-					userId: ctx.session.userId
-				});
+        const subDomain = await tx
+          .insert(subDomains)
+          .values({
+            subDomain: subdomain,
+          })
+          .returning();
 
-				return team;
-			});
+        if (!subDomain[0]) {
+          throw new Error("Subdomain not created");
+        }
 
-			return team;
-		})
+        await tx
+          .update(users)
+          .set({
+            subDomainId: subDomain[0].id,
+          })
+          .where(eq(users.id, ctx.session.userId));
+
+        await tx.insert(teamUsers).values({
+          teamId: team[0].id,
+          userId: ctx.session.userId,
+        });
+
+        return team;
+      });
+
+      return team;
+    }),
 };
