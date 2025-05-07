1ba8fc7c335d3a3ae253e9d1380f8482f5bc8cc4
Luis Felipe <luisrochacruzalves@gmail.com>
2025-05-07 12:24:20 -0300

    🚨 Fix: run linter

--- src/features/leads/constants/skeletonLeadsData.ts
+++ src/features/leads/constants/skeletonLeadsData.ts
@@ -1,23 +1,23 @@
-import type { SkeletonTable } from "~/common/types/skeletonTable.type";
+import type { SkeletonTable } from '~/common/types/skeletonTable.type';
 
 export const skeletonLeadsData = ({ pageSize }: SkeletonTable) =>
-  Array.from({ length: pageSize }, (_, index) => ({
-    id: index,
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    tagId: 0,
-    tag: {
-      id: 0,
-      name: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    image: "/placeholder-avatar.png",
-    status: "",
-    convertedToCustomer: false,
-    convertedToCustomerAt: null,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }));
+	Array.from({ length: pageSize }, (_, index) => ({
+		id: index,
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+		tagId: 0,
+		tag: {
+			id: 0,
+			name: '',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		},
+		image: '/placeholder-avatar.png',
+		status: '',
+		convertedToCustomer: false,
+		convertedToCustomerAt: null,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	}));
