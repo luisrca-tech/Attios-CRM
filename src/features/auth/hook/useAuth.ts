f33e341f1ee984c75be2a9b178f468d68e9057d4
Luis Felipe <luisrochacruzalves@gmail.com>
2025-05-21 23:13:15 -0300

    ✨ Refactor: Standardize import quotes and improve error handling in useAuth hook; enhance user sign-in and sign-up functions for better flow and feedback.

--- src/features/auth/hook/useAuth.ts
+++ src/features/auth/hook/useAuth.ts
@@ -1,230 +1,233 @@
-import { useSignIn, useSignUp } from '@clerk/nextjs';
-import { isClerkAPIResponseError } from '@clerk/nextjs/errors';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { toast } from 'sonner';
-import { api } from '~/trpc/react';
-import type { ConfirmEmail } from '../types/confirmEmail.type';
-import type { RecoverPassword } from '../types/recoverPasssword.type';
-import type { SignInFormType } from '../types/signInForm.type';
-import type { VerifyCode } from '../types/signUpCodeVerify.type';
-import type { SignUpFormType } from '../types/signUpForm.type';
+import { useSignIn, useSignUp } from "@clerk/nextjs";
+import { isClerkAPIResponseError } from "@clerk/nextjs/errors";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import { api } from "~/trpc/react";
+import type { ConfirmEmail } from "../types/confirmEmail.type";
+import type { RecoverPassword } from "../types/recoverPasssword.type";
+import type { SignInFormType } from "../types/signInForm.type";
+import type { VerifyCode } from "../types/signUpCodeVerify.type";
+import type { SignUpFormType } from "../types/signUpForm.type";
 
 export function useAuth() {
-	const router = useRouter();
-	const [emailVerify, setEmailVerify] = useState<boolean>(false);
-	const [verifyCode, setVerifyCode] = useState<boolean>(false);
+  const router = useRouter();
+  const [emailVerify, setEmailVerify] = useState<boolean>(false);
+  const [verifyCode, setVerifyCode] = useState<boolean>(false);
 
-	const {
-		signUp,
-		isLoaded: isSignUpLoaded,
-		setActive: setActiveSignUp
-	} = useSignUp();
-	const {
-		signIn,
-		setActive: setActiveSignIn,
-		isLoaded: isSignInLoaded
-	} = useSignIn();
+  const {
+    signUp,
+    isLoaded: isSignUpLoaded,
+    setActive: setActiveSignUp,
+  } = useSignUp();
+  const {
+    signIn,
+    setActive: setActiveSignIn,
+    isLoaded: isSignInLoaded,
+  } = useSignIn();
 
-	const createUserMutation = api.user.create.useMutation();
+  const createUserMutation = api.user.create.useMutation();
 
-	async function signInUser({ email, password }: SignInFormType) {
-		try {
-			const result = await signIn?.create({
-				identifier: email,
-				password: password
-			});
+  async function signInUser({ email, password }: SignInFormType) {
+    try {
+      const result = await signIn?.create({
+        identifier: email,
+        password: password,
+      });
 
-			if (result?.status === 'complete') {
-				if (!setActiveSignIn) {
-					return null;
-				}
+      if (result?.status === "complete") {
+        if (!setActiveSignIn) {
+          return false;
+        }
 
-				await setActiveSignIn({ session: result.createdSessionId });
-			}
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-		}
-	}
+        await setActiveSignIn({ session: result.createdSessionId });
+        return true;
+      }
+      return false;
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return false;
+    }
+  }
 
-	async function signUpUser({ email, password, fullName }: SignUpFormType) {
-		try {
-			await signUp?.create({
-				emailAddress: email,
-				password,
-				firstName: fullName.split(' ')[0],
-				lastName: fullName.split(' ')[1]
-			});
-			await signUp?.prepareEmailAddressVerification({ strategy: 'email_code' });
-			setEmailVerify(true);
-			return toast.success('Success', {
-				position: 'top-center',
-				description: 'We sent you a verification code to your email.'
-			});
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-			return toast.error('Error', {
-				position: 'top-center',
-				description: 'Something went wrong. Please try again.'
-			});
-		}
-	}
+  async function signUpUser({ email, password, fullName }: SignUpFormType) {
+    try {
+      await signUp?.create({
+        emailAddress: email,
+        password,
+        firstName: fullName.split(" ")[0],
+        lastName: fullName.split(" ")[1],
+      });
+      await signUp?.prepareEmailAddressVerification({ strategy: "email_code" });
+      setEmailVerify(true);
+      return toast.success("Success", {
+        position: "top-center",
+        description: "We sent you a verification code to your email.",
+      });
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        return toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return toast.error("Error", {
+        position: "top-center",
+        description: "Something went wrong. Please try again.",
+      });
+    }
+  }
 
-	async function verifyEmail({ code }: VerifyCode) {
-		if (!isSignUpLoaded) return null;
+  async function verifyEmail({ code }: VerifyCode) {
+    if (!isSignUpLoaded) return null;
 
-		try {
-			const completeSignUp = await signUp?.attemptEmailAddressVerification({
-				code
-			});
+    try {
+      const completeSignUp = await signUp?.attemptEmailAddressVerification({
+        code,
+      });
 
-			if (!completeSignUp.createdUserId) {
-				throw new Error('Id was not provided');
-			}
+      if (!completeSignUp.createdUserId) {
+        throw new Error("Id was not provided");
+      }
 
-			if (completeSignUp.status === 'complete') {
-				const email = completeSignUp.emailAddress;
+      if (completeSignUp.status === "complete") {
+        const email = completeSignUp.emailAddress;
 
-				if (!email) {
-					return toast.error('Error', {
-						position: 'top-center',
-						description: 'Email or name was not provided'
-					});
-				}
+        if (!email) {
+          return toast.error("Error", {
+            position: "top-center",
+            description: "Email or name was not provided",
+          });
+        }
 
-				await createUserMutation.mutateAsync({
-					userId: completeSignUp.createdUserId,
-					email: email,
-					fullName: `${completeSignUp.firstName} ${completeSignUp.lastName}`
-				});
-				await setActiveSignUp({ session: completeSignUp.createdSessionId });
-				return router.push('/');
-			}
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-			return toast.error('Error', {
-				position: 'top-center',
-				description: 'Something went wrong. Please try again.'
-			});
-		}
-	}
+        await createUserMutation.mutateAsync({
+          userId: completeSignUp.createdUserId,
+          email: email,
+          fullName: `${completeSignUp.firstName} ${completeSignUp.lastName}`,
+        });
+        await setActiveSignUp({ session: completeSignUp.createdSessionId });
+        return router.push("/");
+      }
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        return toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return toast.error("Error", {
+        position: "top-center",
+        description: "Something went wrong. Please try again.",
+      });
+    }
+  }
 
-	async function resendCode() {
-		if (!isSignUpLoaded) return null;
-		try {
-			await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
-			return toast.success('Success', {
-				position: 'top-center',
-				description: 'We resent you a verification code to your email.'
-			});
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-			return toast.error('Error', {
-				position: 'top-center',
-				description: 'Something went wrong. Please try again.'
-			});
-		}
-	}
+  async function resendCode() {
+    if (!isSignUpLoaded) return null;
+    try {
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      return toast.success("Success", {
+        position: "top-center",
+        description: "We resent you a verification code to your email.",
+      });
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        return toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return toast.error("Error", {
+        position: "top-center",
+        description: "Something went wrong. Please try again.",
+      });
+    }
+  }
 
-	const confirmEmailBeforeResetPassword = async ({ email }: ConfirmEmail) => {
-		try {
-			await signIn?.create({
-				identifier: email,
-				strategy: 'reset_password_email_code'
-			});
+  const confirmEmailBeforeResetPassword = async ({ email }: ConfirmEmail) => {
+    try {
+      await signIn?.create({
+        identifier: email,
+        strategy: "reset_password_email_code",
+      });
 
-			setVerifyCode(true);
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-			return toast.error('Error', {
-				position: 'top-center',
-				description: 'Something went wrong. Please try again.'
-			});
-		}
-	};
+      setVerifyCode(true);
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        return toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return toast.error("Error", {
+        position: "top-center",
+        description: "Something went wrong. Please try again.",
+      });
+    }
+  };
 
-	const handleRecoverPassword = async ({ password, code }: RecoverPassword) => {
-		try {
-			const completeRecoverPassword = await signIn?.attemptFirstFactor({
-				strategy: 'reset_password_email_code',
-				code,
-				password
-			});
+  const handleRecoverPassword = async ({ password, code }: RecoverPassword) => {
+    try {
+      const completeRecoverPassword = await signIn?.attemptFirstFactor({
+        strategy: "reset_password_email_code",
+        code,
+        password,
+      });
 
-			if (completeRecoverPassword?.status === 'complete') {
-				if (!setActiveSignIn) {
-					return null;
-				}
-				await setActiveSignIn({
-					session: completeRecoverPassword.createdSessionId
-				});
-			}
-		} catch (error) {
-			if (isClerkAPIResponseError(error)) {
-				return toast.error('Error', {
-					position: 'top-center',
-					description: error?.errors[0]?.longMessage
-				});
-			}
-			return toast.error('Error', {
-				position: 'top-center',
-				description: 'Something went wrong. Please try again.'
-			});
-		}
-	};
+      if (completeRecoverPassword?.status === "complete") {
+        if (!setActiveSignIn) {
+          return null;
+        }
+        await setActiveSignIn({
+          session: completeRecoverPassword.createdSessionId,
+        });
+      }
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) {
+        return toast.error("Error", {
+          position: "top-center",
+          description: error?.errors[0]?.longMessage,
+        });
+      }
+      return toast.error("Error", {
+        position: "top-center",
+        description: "Something went wrong. Please try again.",
+      });
+    }
+  };
 
-	const signInWithGoogle = () =>
-		signIn?.authenticateWithRedirect({
-			strategy: 'oauth_google',
-			redirectUrl: '/sso-callback',
-			redirectUrlComplete: '/'
-		});
+  const signInWithGoogle = () =>
+    signIn?.authenticateWithRedirect({
+      strategy: "oauth_google",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/",
+    });
 
-	const signInWithFacebook = () =>
-		signIn?.authenticateWithRedirect({
-			strategy: 'oauth_facebook',
-			redirectUrl: '/sso-callback',
-			redirectUrlComplete: '/'
-		});
+  const signInWithFacebook = () =>
+    signIn?.authenticateWithRedirect({
+      strategy: "oauth_facebook",
+      redirectUrl: "/sso-callback",
+      redirectUrlComplete: "/",
+    });
 
-	return {
-		signUpUser,
-		signInUser,
-		emailVerify,
-		isSignUpLoaded,
-		isSignInLoaded,
-		verifyEmail,
-		resendCode,
-		confirmEmailBeforeResetPassword,
-		handleRecoverPassword,
-		verifyCode,
-		isLoading: createUserMutation.isPending,
-		signInWithGoogle,
-		signInWithFacebook
-	};
+  return {
+    signUpUser,
+    signInUser,
+    emailVerify,
+    isSignUpLoaded,
+    isSignInLoaded,
+    verifyEmail,
+    resendCode,
+    confirmEmailBeforeResetPassword,
+    handleRecoverPassword,
+    verifyCode,
+    isLoading: createUserMutation.isPending,
+    signInWithGoogle,
+    signInWithFacebook,
+  };
 }
