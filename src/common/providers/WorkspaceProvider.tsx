037637624605d044e4c47b8aae9d13f211f324ff
Luis Felipe <luisrochacruzalves@gmail.com>
2025-06-03 08:42:49 -0300

    🔧 Refactor: Replace useSubdomain hook with useWorkspace in WorkspaceProvider; remove obsolete useSubdomain logic.

--- src/common/providers/WorkspaceProvider.tsx
+++ src/common/providers/WorkspaceProvider.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { useAtom } from "jotai";
-import { useSubdomain } from "~/features/subdomain/hooks/useSubdomain";
+import { useWorkspace } from "~/features/subdomain/hooks/useWorkspace";
 import { workspaceAtom } from "../atoms/current-workspace";
 import { useEffect } from "react";
 
 export function WorkspaceProvider({ children }: { children: React.ReactNode }) {
-  const { subdomain } = useSubdomain();
+  const { workspace } = useWorkspace();
   const [, setWorkspace] = useAtom(workspaceAtom);
 
   useEffect(() => {
-    if (subdomain) {
-      setWorkspace(subdomain);
+    if (workspace) {
+      setWorkspace(workspace);
     }
-  }, [subdomain, setWorkspace]);
+  }, [workspace, setWorkspace]);
 
   return <>{children}</>;
 }
--- src/features/subdomain/hooks/useSubdomain.ts
+++
@@ -1,8 +0,0 @@
-export function useSubdomain() {
-  const url = window.location.hostname;
-  const splitUrl = url.split(".");
-  const hasSubdomain = splitUrl.length >= 2 && splitUrl[0] !== "www";
-  const subdomain = hasSubdomain ? splitUrl[0] : null;
-
-  return { subdomain };
-}
