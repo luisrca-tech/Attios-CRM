c99ef872b8f63a312529ac5176888b71ebdd2d23
Luis Felipe <luisrochacruzalves@gmail.com>
2025-06-03 10:10:44 -0300

    🚨 Fix: run linter

--- src/features/teams/hooks/teams.hook.ts
+++ src/features/teams/hooks/teams.hook.ts
@@ -1,30 +1,30 @@
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
-import { api } from "~/trpc/react";
-import { getWorkspaceDomain } from "~/utils/workspace";
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+import { api } from '~/trpc/react';
+import { getWorkspaceDomain } from '~/utils/workspace';
 
 type UseTeamsProps = {
-  isFirstTeam?: boolean;
+	isFirstTeam?: boolean;
 };
 
 export const useTeams = ({ isFirstTeam = false }: UseTeamsProps) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const createTeam = api.workspace.create.useMutation({
-    onSuccess: (data) => {
-      toast.success("Team and subdomain created successfully!");
-      if (isFirstTeam) {
-        router.refresh();
-        const domain = getWorkspaceDomain(data.workspace);
-        router.push(`${domain}/sign-in`);
-      }
-    },
-    onError: () => {
-      toast.error("Failed to create team and workspace. Please try again.");
-    },
-  });
+	const createTeam = api.workspace.create.useMutation({
+		onSuccess: (data) => {
+			toast.success('Team and subdomain created successfully!');
+			if (isFirstTeam) {
+				router.refresh();
+				const domain = getWorkspaceDomain(data.workspace);
+				router.push(`${domain}/sign-in`);
+			}
+		},
+		onError: () => {
+			toast.error('Failed to create team and workspace. Please try again.');
+		}
+	});
 
-  return {
-    createTeam,
-  };
+	return {
+		createTeam
+	};
 };
--- src/server/api/routers/user/user.ts
+++ src/server/api/routers/user/user.ts
@@ -1,30 +1,30 @@
-import { eq } from "drizzle-orm";
-import { z } from "zod";
-import { users } from "~/server/db/schema";
-import { createTRPCRouter, publicProcedure } from "../../trpc";
-import { userSchema } from "./schemas/user.schema";
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+import { users } from '~/server/db/schema';
+import { createTRPCRouter, publicProcedure } from '../../trpc';
+import { userSchema } from './schemas/user.schema';
 
 export const userRouter = createTRPCRouter({
-  create: publicProcedure.input(userSchema).mutation(async ({ ctx, input }) => {
-    await ctx.db.insert(users).values({
-      id: input.userId,
-      email: input.email,
-      fullName: input.fullName,
-    });
+	create: publicProcedure.input(userSchema).mutation(async ({ ctx, input }) => {
+		await ctx.db.insert(users).values({
+			id: input.userId,
+			email: input.email,
+			fullName: input.fullName
+		});
 
-    return { success: true };
-  }),
+		return { success: true };
+	}),
 
-  getUserById: publicProcedure
-    .input(z.string())
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.db.query.users.findFirst({
-        where: eq(users.id, input),
-        with: {
-          workspaces: true,
-          teams: true,
-        },
-      });
-      return user;
-    }),
+	getUserById: publicProcedure
+		.input(z.string())
+		.query(async ({ ctx, input }) => {
+			const user = await ctx.db.query.users.findFirst({
+				where: eq(users.id, input),
+				with: {
+					workspaces: true,
+					teams: true
+				}
+			});
+			return user;
+		})
 });
